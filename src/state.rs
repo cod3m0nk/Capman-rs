6af8813792de49671bd816240441b339109e405f
cod3m0nk <75347870+cod3m0nk@users.noreply.github.com>
2024-02-23 12:24:00 +0100

    Refactor to use Bevy run conditions

--- src/state.rs
+++ src/state.rs
@@ -12,8 +12,10 @@ pub struct StatePlugin;
 
 impl Plugin for StatePlugin {
     fn build(&self, app: &mut App) {
-        app.init_state::<GameState>()
-            .add_systems(Update, game_state_input_events);
+        app.init_state::<GameState>().add_systems(
+            Update,
+            game_state_input_events.run_if(on_event::<GameStateChangeEvent>()),
+        );
     }
 }
 
@@ -27,13 +29,10 @@ pub struct GameGlobals {
 fn game_state_input_events(
     mut next_state: ResMut<NextState<GameState>>,
     state: Res<State<GameState>>,
-    mut input_event_reader: EventReader<GameStateChangeEvent>,
 ) {
-    for _ in input_event_reader.read() {
-        if *state.get() == GameState::Running {
-            next_state.set(GameState::Paused);
-        } else {
-            next_state.set(GameState::Running);
-        }
+    if *state.get() == GameState::Running {
+        next_state.set(GameState::Paused);
+    } else {
+        next_state.set(GameState::Running);
     }
 }
